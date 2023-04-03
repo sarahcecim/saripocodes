71c5a7c344bbcba6a1433f5cbc75d8e5d692b44e
sarahcecim <111818953+sarahcecim@users.noreply.github.com>
2023-04-03 18:02:15 -0300

    Add files via upload

---
+++ mobile-navbar.js
@@ -0,0 +1,17 @@
+class MobiçeNavbar {
+    constructor() {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelector(navLinks);
+        this.activeClass = "active";
+    }
+
+    addClickEvent()
+    this.mobileMenu.addEventListener("click", () => console.log
+    ("Hey!"));
+    }
+
+    Init() {
+        if (this.mobileMenu) 
+    }
+
